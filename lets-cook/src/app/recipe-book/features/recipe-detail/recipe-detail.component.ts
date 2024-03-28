72189d4709991127419f14be5893e338d09781d0
Motheo Moiloa <motheo@bbd.co.za>
2024-03-28 12:01:44 +0200

    Update recipe detail, recipe item and recip list

--- lets-cook/src/app/recipe-book/features/recipe-detail/recipe-detail.component.ts
+++ lets-cook/src/app/recipe-book/features/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Recipe } from '../../data-access/models/recipe.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,9 +8,11 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
+  recipeName?: string;
+
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    //this.recipeName = this.route.snapshot.params['name'];
+    this.recipeName = this.route.snapshot.params['name'];
   }
 }
