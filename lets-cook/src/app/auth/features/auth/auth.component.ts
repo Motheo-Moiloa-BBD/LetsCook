a84db59229c86747457556a0c59672cd6cec08a8
Motheo Moiloa <motheo@bbd.co.za>
2024-04-12 16:09:27 +0200

    Add alert modal dynamic component

--- lets-cook/src/app/auth/features/auth/auth.component.ts
+++ lets-cook/src/app/auth/features/auth/auth.component.ts
@@ -1,21 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { passwordValidator } from '../../utils/functions/password-validator.validator';
 import { AuthService } from '../../data-access/services/auth.service';
 import { Auth } from '../../data-access/models/auth.model';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { AuthResponse } from '../../data-access/models/auth-response.model';
+import { AlertComponent } from 'src/app/shared/ui/alert/alert.component';
+import { PlaceHolderDirective } from 'src/app/shared/utils/directives/place-holder.directive';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css'],
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
   isLoginMode: boolean = true;
   submitted: boolean = false;
   error: string = '';
   isLoading: boolean = false;
+  @ViewChild(PlaceHolderDirective, { static: false })
+  alertHost?: PlaceHolderDirective;
+  private closeSubscription?: Subscription;
 
   authForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -60,6 +65,7 @@ export class AuthComponent {
         },
         error: (error: Error) => {
           this.error = error.message;
+          this.showErrorAlert(error.message);
           this.isLoading = false;
         },
       });
@@ -68,8 +74,36 @@ export class AuthComponent {
     }
   }
 
+  onHandleError() {
+    this.error = '';
+  }
+
+  private showErrorAlert(message: string) {
+    if (this.alertHost) {
+      const hostViewContainerRef = this.alertHost.viewContainerRef;
+      hostViewContainerRef.clear();
+
+      const componentRef =
+        hostViewContainerRef.createComponent<AlertComponent>(AlertComponent);
+
+      componentRef.instance.message = message;
+      this.closeSubscription = componentRef.instance.close.subscribe(() => {
+        if (this.closeSubscription) {
+          this.closeSubscription.unsubscribe();
+          hostViewContainerRef.clear();
+        }
+      });
+    }
+  }
+
   resetForm() {
     this.authForm.reset();
     this.submitted = false;
+  }
+
+  ngOnDestroy(): void {
+    if (this.closeSubscription) {
+      this.closeSubscription.unsubscribe();
+    }
   }
 }
--- lets-cook/src/app/shared/shared.module.ts
+++ lets-cook/src/app/shared/shared.module.ts
@@ -5,10 +5,23 @@ import { RouterModule } from '@angular/router';
 import { DropDownDirective } from './utils/directives/drop-down.directive';
 import { HttpClientModule } from '@angular/common/http';
 import { LoadingSpinnerComponent } from './ui/loading-spinner/loading-spinner.component';
+import { AlertComponent } from './ui/alert/alert.component';
+import { PlaceHolderDirective } from './utils/directives/place-holder.directive';
 
 @NgModule({
-  declarations: [NavBarComponent, DropDownDirective, LoadingSpinnerComponent],
+  declarations: [
+    NavBarComponent,
+    DropDownDirective,
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceHolderDirective,
+  ],
   imports: [CommonModule, RouterModule, HttpClientModule],
-  exports: [DropDownDirective, LoadingSpinnerComponent],
+  exports: [
+    DropDownDirective,
+    PlaceHolderDirective,
+    LoadingSpinnerComponent,
+    AlertComponent,
+  ],
 })
 export class SharedModule {}
